566e2722942cde276b69db0bdae22e650a33adf8
Georg Rabenstein <georg@oak.tt>
2021-02-19 12:51:42 +0100

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 output "private_endpoints" {
-  value = var.peering_enabled ? mongodbatlas_cluster.peer_database.connection_strings : mongodbatlas_cluster.no_peer_database.connection_strings
+  value = var.peering_enabled ? mongodbatlas_cluster.peer_database[0].connection_strings : mongodbatlas_cluster.no_peer_database[0].connection_strings
 }
 output "mongo_app_user_password" {
   value = [random_string.user_password[*].result]
